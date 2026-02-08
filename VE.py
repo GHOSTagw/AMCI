8ccd7fbec2f04e49c318328cc1160e53c02ba979
PAK43 <40930046+GHOSTagw@users.noreply.github.com>
2026-02-08 16:15:38 +0800

    Change User-Agent to Firefox on Windows
    Updated User-Agent header to use Firefox on Windows.

--- VE.py
+++ VE.py
@@ -11,11 +11,7 @@ TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 HEADERS = {
     'Connection': 'keep-alive',
     'Accept': 'application/json, text/plain, */*',
-    'User-Agent': (
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
-        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 '
-        'Safari/537.36'
-    ),
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0',
     'X-Requested-With': 'XMLHttpRequest',
     'Sec-Fetch-Site': 'same-origin',
     'Sec-Fetch-Mode': 'cors',
